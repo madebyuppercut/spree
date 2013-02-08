063f78f6af64c49505a22cc403b7d05f2735e18a
Tyler Mercier <tylermercier@gmail.com>
2013-02-08 10:53:13 -0700

    specify ffaker version, ignore mysql

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -5,7 +5,7 @@ source 'http://rubygems.org'
 
 gem 'json'
 gem 'sqlite3'
-gem 'mysql2'
+#gem 'mysql2'
 gem 'pg'
 gem 'multi_json', "1.2.0"
 # Gems used only for assets and not required
@@ -20,7 +20,7 @@ group :test do
   gem 'factory_girl_rails', '~> 1.7.0'
   gem 'email_spec', '~> 1.2.1'
 
-  gem 'ffaker'
+  gem 'ffaker', '~> 1.12.0'
   gem 'shoulda-matchers', '~> 1.0.0'
   gem 'capybara', '1.1.3'
   gem 'selenium-webdriver', '2.27.1'
